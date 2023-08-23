c40c9153cc80b5dec27d991f66b7a57eeb91e3dd
Ismoil <ismoilanvarov26@gmail.com>
2023-08-24 00:49:56 +0500

    feat: created half of the gallery section

--- src/pages/HomePage/HomeSections/GallerySection.vue
+++ src/pages/HomePage/HomeSections/GallerySection.vue
@@ -1,13 +1,124 @@
 <template>
-  
+  <section>
+    <div class="container py-10">
+
+      <div class="titles text-center">
+        <h3 class="text-md lg:text-xl">Bizning loyihalaramiz</h3>
+        <h1 class="text-2xl lg:text-5xl my-3 text-darker-green">So'nggi ishlarimiz</h1>
+      </div>
+
+      <div class="btns flex items-center justify-center gap-5">
+        <button v-for="item in categoryArray" :key="item" :value="item.text" @click="filterCards(item.text)"
+          class="btn flex items-center text-lg capitalize"
+          :class="{ 'text-green-700': isFiltered === '' || item.text === isFiltered }">
+          {{ item.text }}
+        </button>
+      </div>
+
+      <div class="wrapper flex items-center justify-center flex-wrap gap-5 my-10">
+
+        <!-- card -->
+        <div class="card w-[240px] md:w-[295px] lg:w-[350px] lg:h-[285px] overflow-hidden transition-all relative"
+          v-for="(item, idx) in galleryArray" :key="item" v-show="isFiltered === '' || item.category === isFiltered"
+          @click="openModal(idx)">
+          <div class="image-block">
+            <img :src="item.image" alt="#" class="w-full h-full">
+          </div>
+          <div class="bottom text-white flex items-end absolute left-0 bottom-0 z-10 w-full">
+            <div class="icon w-[85px] h-[65px] bg-green-900"></div>
+            <div class="text py-4 pl-4 sm:px-4 w-full bg-darker-green/80">
+              <h1 class="">{{ item.name }}</h1>
+            </div>
+          </div>
+          <div class="modal" v-if="isModalOpened">
+            <TheModal :info="modalData" @closeModal="closeModal" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {
+import TheModal from "../components/GalleryModal.vue"
 
+export default {
+  components: {
+    TheModal
+  },
+  data() {
+    return {
+      isModalOpened: false,
+      isFiltered: "",
+      modalData: {},
+      categoryArray: [
+        {
+          text: 'barchasi'
+        },
+        {
+          text: 'gazon'
+        },
+        {
+          text: 'poliv'
+        },
+
+      ],
+      galleryArray: [
+        {
+          name: `Islom Sivilizatsiyasi`,
+          category: 'gazon',
+          image: 'src/assets/images/islom.jpg',
+          toModal: true,
+        },
+        {
+          name: `Milliy bog'`,
+          category: 'poliv',
+          image: 'src/assets/images/milliy.png',
+          toModal: true,
+        },
+      ]
+    }
+  },
+  methods: {
+    filterCards(name) {
+      this.isFiltered = name === 'barchasi' ? '' : name;
+    },
+    openModal(idx) {
+      this.modalData = this.galleryArray[idx];
+      this.isModalOpened = true;
+    },
+    closeModal() {
+      this.isModalOpened = false;
+      this.modalData = null;
+    },
+  }
 }
 </script>
 
-<style>
+<style scoped>
+.card img {
+  transition: 0.3s;
+}
 
+.card img:hover {
+  scale: 1.2;
+}
+
+.btn:first-child::after {
+  content: '/';
+  position: relative;
+  left: 0;
+  top: 0;
+  padding-left: 10px;
+  font-size: 25px;
+}
+
+.btn:last-child::before {
+  content: '/';
+  position: relative;
+  left: 0;
+  top: 0;
+  padding-right: 10px;
+  font-size: 25px;
+}
 </style>
---
+++ src/pages/HomePage/components/GalleryModal.vue
@@ -0,0 +1,52 @@
+<template>
+  <!-- Main modal -->
+  <div
+    class="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative w-full max-w-2xl max-h-full">
+      <!-- Modal content -->
+      <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+        <!-- Modal header -->
+        <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+          <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+            {{ info.name }}
+          </h3>
+          <button @click="$emit('closeModal')"
+            class="text-gray-900 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center">
+            <i class="fa-solid fa-xmark text-2xl"></i>
+          </button>
+        </div>
+        <!-- Modal body -->
+        <div class="p-6 space-y-6">
+          <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+            The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant
+            to ensure a common set of data rights in the European Union. It requires organizations to notify users as
+            soon as possible of high-risk data breaches that could personally affect them.
+          </p>
+        </div>
+        <!-- Modal footer -->
+        <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+          <button data-modal-hide="staticModal" type="button"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I
+            accept</button>
+          <button @click="$emit('closeModal')" data-modal-hide="staticModal" type="button"
+            class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  props: {
+    info: {
+      type: Object
+    }
+  }
+}
+</script>
